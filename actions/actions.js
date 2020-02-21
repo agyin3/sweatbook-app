ba1d7a08fe15e4b0dbaf1b43028d475e21fadb5d
Buddy Agyin <agyin3@gmail.com>
2020-02-20 23:23:56 -0700

    add exercise filter functionality

--- actions/actions.js
+++ actions/actions.js
@@ -3,3 +3,8 @@ import * as types from './types.js'
 export const setExerciseModalVisibilty = (visible) => dispatch =>  {
     dispatch({type: types.SET_EXERCISE_VISIBILTY, payload: visible})
 }
+
+export const filterExercises = (filter) => dispatch => {
+    console.log(filter)
+    dispatch({type: types.FILTER_EXERCISES, payload: filter})
+}
